a6eeb5dee7976cee9f48f82e863f7ab4c551e566
Netanel Mugrabi <108195904+NetanelMu@users.noreply.github.com>
2024-12-20 22:59:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+$(".travel a").hover(function () {
+  $(".text1").addClass("show");
+});
+
+$(".travel a").mouseleave(function () {
+  $(".text1").removeClass("show");
+});
+
+$(".Learning a").hover(function () {
+  $(".text2").addClass("show");
+});
+
+$(".Learning a").mouseleave(function () {
+  $(".text2").removeClass("show");
+});
+
+$(".Food a").hover(function () {
+  $(".text3").addClass("show");
+});
+
+$(".Food a").mouseleave(function () {
+  $(".text3").removeClass("show");
+});
